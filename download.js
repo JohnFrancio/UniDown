0133977eafdfd46c9757e93cb69db0a724febe35
JohnFrancio <franciaw@gmail.com>
2024-11-04 22:04:25 +0300

    first commit

---
+++ download.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const axios = require("axios");
+const path = require("path");
+const nayan = require("nayan-media-downloader");
+const os = require("os");
+
+const osPath = `${os.homedir()}/Downloads/uni-down`;
+fs.mkdirSync(osPath, { recursive: true });
+
+const { instagram, ndown, tikdown, ytdown } = nayan;
+// https://youtube.com/shorts/nYXrsXRmpy0?si=qUkAI5YNxo5Ik1h1
+// https://youtu.be/HOtgFGIBL3g?si=LOyOnFnT27jJ-pP6
+async function chooseUrl(url) {
+  let data, res, type;
+  let choice = url.includes("facebook.com")
+    ? "facebook"
+    : url.includes("instagram.com")
+    ? "instagram"
+    : url.includes("tiktok.com")
+    ? "tiktok"
+    : url.includes("youtube.com") || url.includes("youtu.be")
+    ? "youtube"
+    : "";
+  switch (choice) {
+    case "instagram":
+      data = await instagram(url);
+      return data.data.video;
+    case "facebook":
+      data = await ndown(url);
+      return data.data[0].url;
+    case "tiktok":
+      data = await tikdown(url);
+      return data.data.video;
+    case "youtube":
+      data = await ytdown(url);
+      return data.data.video;
+    default:
+      return "Something went wrong while downloading your video.";
+  }
+}
+
+async function downloadFile(fileUrl, resolution) {
+  const fileName = `video_${resolution}_${Date.now()}.mp4`;
+  const filePath = path.resolve(osPath, fileName);
+  const response = await axios.get(fileUrl, { responseType: "stream" });
+
+  response.data.pipe(fs.createWriteStream(filePath));
+  // console.log(`Downloading ${resolution}...`);
+
+  return new Promise((resolve, reject) => {
+    response.data.on("end", () => {
+      // console.log(`Downloaded ${fileName}`);
+      resolve();
+    });
+    response.data.on("error", reject);
+  });
+}
+
+module.exports = {
+  chooseUrl,
+  downloadFile,
+};
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const { chooseUrl, downloadFile } = require("./download");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+const HOSTNAME = process.env.NAME;
+
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json());
+
+app.use(express.json()); // For parsing JSON request bodies
+
+// Route to download a video from a chosen platform
+app.post("/download", async (req, res) => {
+  const { url } = req.body;
+
+  if (!url) {
+    return res.status(400).json({ error: "Platform and URL are required." });
+  }
+
+  try {
+    // Choose the URL based on the platform
+    const downloadUrl = await chooseUrl(url);
+
+    // Start the download process
+    await downloadFile(downloadUrl, "720 HD");
+    res.status(200).json({ message: "Video Downloaded!" });
+  } catch (error) {
+    console.error("Error downloading file:", error);
+    res.status(500).json({ error: "Failed to download the file." });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`API server is running on http://${HOSTNAME}:${PORT}`);
+});
